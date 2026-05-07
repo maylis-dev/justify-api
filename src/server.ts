a62c98ed35ed24bc2919711894c1e9da99dfa0aa
maylis imperiali <imp.mayli@gmail.com>
2026-05-07 09:35:06 +0200

    host

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.get("/", (_, res) => {
   res.send("API running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
